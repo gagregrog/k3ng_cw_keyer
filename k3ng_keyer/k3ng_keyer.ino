5e60bee9a20e7d262a1d3a704cc5a6ce59039af4
w7yfr <w7yfr.73@gmail.com>
2026-07-25 15:45:32 -0700

    fix: only first button triggers lcd reset

--- k3ng_keyer/k3ng_keyer.ino
+++ k3ng_keyer/k3ng_keyer.ino
@@ -9622,7 +9622,7 @@ void extra_button_pressed(byte extra_button_number, boolean is_hold, boolean is_
   //     (always false when is_command_mode is true - command_mode() has no hold concept)
   //   is_command_mode: true if the button was pressed while in command_mode()
 
-  if (!is_command_mode) {
+  if (!is_command_mode && extra_button_number == 0) {
     if (is_hold) {
       ssd1306_toggle_display();
     } else {
