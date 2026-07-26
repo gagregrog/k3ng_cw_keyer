66073b7aa8d1d540037049ddece8fdfdb420518b
w7yfr <w7yfr.73@gmail.com>
2026-07-25 19:53:44 -0700

    fix: propagate OPTION_SKIP_SAVED_MEMORY_PLAYBACK to remaining board profiles
    open_interface and test_everything were missed when this option was added
    to the other 20 hardware-specific keyer_features_and_options_*.h files.

--- k3ng_keyer/keyer_features_and_options_open_interface.h
+++ k3ng_keyer/keyer_features_and_options_open_interface.h
@@ -112,6 +112,7 @@
 // #define OPTION_PERSONALIZED_STARTUP_SCREEN        // displays a user defined string of characters on the second or fourth row of the screen during startup. 1602 display requires OPTION_DO_NOT_SAY_HI
 // #define OPTION_SWAP_PADDLE_PARAMETER_CHANGE_DIRECTION        // reverses the up/down direction when using the paddles to change the wpm or sidetone frequency
 #define OPTION_DISPLAY_MEMORY_CONTENTS_COMMAND_MODE
+// #define OPTION_SKIP_SAVED_MEMORY_PLAYBACK // skips playback of the memory after recording it
 
 // #define OPTION_BEACON_MODE_MEMORY_REPEAT_TIME        // to space out the repeated playing of memory 1 when in beacon mode
 // #define OPTION_BEACON_MODE_PTT_TAIL_TIME             // adds the ptt tail time to each playing of memory 1 in beacon mode
